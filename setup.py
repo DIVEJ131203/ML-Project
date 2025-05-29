83c6eb2934ace81fe6daadc7f2fa6d8460518cbd
Divej Ahuja <ahujadivej@gmail.com>
2025-05-29 11:19:47 +0530

    Fix: custom exception and logging configuration

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_requirements(file_path: str) -> List[str]:
 
 setup(
     name='mlproject',
-    version='0.0.1',
+    version='0.0.1', 
     author="divej",
     author_email="divejahuja@gmail.com",
     packages=find_packages(),
